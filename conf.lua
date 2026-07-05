2e22847cca1e1c2b416ae305abe24a5b93eb88d2
DogeCalc32 <andersondotlincoln@gmail.com>
2026-07-05 14:25:22 -0600

    changed to 2d game

--- conf.lua
+++ conf.lua
@@ -2,5 +2,5 @@ function love.conf(t)
     t.window.width = 1280
     t.window.height = 720
     t.window.title = "Raycasting"
-    t.console = true
+    -- t.console = true
 end
--- main.lua
+++ main.lua
@@ -1,28 +1,66 @@
+--main.lua
 function love.load()
     tileSize = 32
     map = {
-        {1,1,1,1,1,1},
-        {1,0,0,1,1,1},
-        {1,0,0,1,1,1},
-        {1,0,0,0,0,1},
-        {1,2,0,0,0,1},
-        {1,1,1,1,1,1},
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        {1,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
     }
 
     love.graphics.setDefaultFilter("nearest", "nearest")
-    steveSprite = love.graphics.newImage("assets/steve.png")
+    playerSprite = love.graphics.newImage("assets/player.png")
     wallSprite = love.graphics.newImage("assets/wall.png")
-    sprites = {wallSprite,steveSprite}
+    sprites = {wallSprite,playerSprite}
 
     player = {
         x = 2,
         y = 2,
-        speed = 1,
-        fov = math.rad(60),
+        speed = .2,
+        sprint = 1,
     }
 end
 
+function canCollide()
+    -- put collision logic here
+end
+
 function love.update(dt)
+    if love.keyboard.isDown("lshift") then -- sprint key
+        if love.keyboard.isDown("w" or "up") then
+            player.y = player.y - player.sprint * dt
+        end
+        if love.keyboard.isDown("a" or "left") then
+            player.x = player.x - player.sprint * dt
+        end
+
+        if love.keyboard.isDown("s" or "down") then
+            player.y = player.y + player.sprint * dt
+        end
+        if love.keyboard.isDown("d" or "right") then
+            player.x = player.x + player.sprint * dt
+        end
+    else -- normal walk
+        if love.keyboard.isDown("w" or "up") then
+            player.y = player.y - player.speed * dt
+        end
+        if love.keyboard.isDown("a" or "left") then
+            player.x = player.x - player.speed * dt
+        end
+
+        if love.keyboard.isDown("s" or "down") then
+            player.y = player.y + player.speed * dt
+        end
+        if love.keyboard.isDown("d" or "right") then
+            player.x = player.x + player.speed * dt
+        end
+    end
 end
 
 function love.draw()
@@ -38,10 +76,6 @@ function love.draw()
                 local scaleY =  tileSize / imageHeight
 
                 love.graphics.draw(sprite,x,y,nil,scaleX,scaleY)
-                -- love.graphics.print("testing",10,10)
-                if column == 2 then
-                    print("testing ".. scaleX)
-                end
             end
         end
     end
