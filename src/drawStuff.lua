5672a6379fee332f2a3106686aec728b3767b5d1
DogeCalc32 <andersondotlincoln@gmail.com>
2026-07-07 09:02:52 -0600

    added multi-layer support

--- src/drawStuff.lua
+++ src/drawStuff.lua
@@ -3,7 +3,23 @@
 drawStuff = {}
 
 function drawStuff.drawMap()
-    for rowIndex, row in ipairs(map) do
+    for rowIndex, row in ipairs(map.layer1) do
+        for columnIndex, column in ipairs(row) do
+            if column > 0 then
+                love.graphics.setColor(1,1,1)
+                local sprite = sprites[column]
+
+                local x = columnIndex * tileSize
+                local y = rowIndex * tileSize
+                local imageWidth, imageHeight = sprite:getDimensions()
+                local scaleX =  tileSize / imageWidth
+                local scaleY =  tileSize / imageHeight
+
+                love.graphics.draw(sprite,x,y,nil,scaleX,scaleY)
+            end
+        end
+    end
+    for rowIndex, row in ipairs(map.layer2) do
         for columnIndex, column in ipairs(row) do
             if column > 0 then
                 love.graphics.setColor(1,1,1)
@@ -31,7 +47,7 @@ function drawStuff.drawPlayer()
     love.graphics.rectangle("fill", 10,10, batterySizeX,batterySizeY)
     love.graphics.setColor(0,1,0)
     love.graphics.rectangle("fill", 16,16, player.stamina * 6, batterySizeY - 11)
-
+    love.graphics.print("FPS: ".. love.timer.getFPS(),5, h/2)
 end
 
 return drawStuff
---
+++ src/updateStuff.lua
@@ -0,0 +1,1 @@
+-- updateStuff.lua
